4d9ab9f3fed50577101ebc21d2be6bb15f29b97c
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-02-01 11:43:21 -0800

    Added the dance ability!  Everything is contained in one loop with function calls. Time for a walk

--- src/index.js
+++ src/index.js
@@ -15,20 +15,38 @@ function createButton(foodCategory, type, parent) {
             break;
 
         case 'dance':
+            newButton.classList.add(foodCategory);
+            newButton.addEventListener('click', function(){
+                danceAction(foodCategory);
+            })
             break;
     }
     parent.appendChild(newButton);
 }
 
-function feedMonster(food) {
+function feedMonster(foodCategory) {
     const bodySegment = document.createElement('span');
     const monsterContainer = document.getElementById('monster-container');
-    bodySegment.classList.add('segment',food);
+    bodySegment.classList.add('segment',foodCategory);
     monsterContainer.appendChild(bodySegment);
+}
+
+function danceAction(foodCategory) {
+    //look for dancing parts, remove
+    const dancingParts = document.querySelectorAll('.dance');
+    const selected = document.querySelectorAll('.segment.'+ foodCategory);
+    //Look for selected parts, make dance
+    for(let i=0; i<dancingParts.length; i++) {
+        dancingParts[i].classList.remove('dance');
+    }
+    for(let i=0; i<selected.length; i++) {
+        selected[i].classList.add('dance');
+    }
 }
 
 
 for(let i = 0; i < foodArray.length; i++) {
     const food = foodArray[i];
     createButton(food,'food',feedButtonContainer);
+    createButton(food,'dance',danceButtonContainer);
 }
